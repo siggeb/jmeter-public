6cb62912324ad6238f7dc7771fa4681837d359ac
Sigurdur Birgisson <siggge@gmail.com>
2017-10-31 14:52:26 +0100

    add args to run

--- elasticsearch-jmeter/testpack/run.sh
+++ elasticsearch-jmeter/testpack/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm /opt/jmeter/report.jtl
 rm -r /opt/jmeter/html_report
-/opt/jmeter/apache-jmeter-3.1/bin/jmeter -n -r -t /opt/jmeter/Na-KD_ssr_Prodhttp_JMeter_PhantomJS.jmx -DPhantomJS_Driver="$(which phantomjs)" -l /opt/jmeter/report.jtl -Grun.properties -Jm1=10.0.1.4 -Jm2=10.0.1.5 -Jm3=10.0.1.6 -Jm4=10.0.1.7 -Jm5=10.0.1.8 -Jreport1=report1 -Jreport2=report2 -Jreport3=report3 -Jreport4=report4 -Jreport5=report5 -Jreport6=report6 -Jreport7=report7 -Jreport8=report8 -Jreport9=report9
+JVM_ARGS="-Xms1024m -Xmx6144m -XX:NewSize=512m -XX:MaxNewSize=6144m" && export JVM_ARGS && /opt/jmeter/apache-jmeter-3.1/bin/jmeter -n -r -t /opt/jmeter/Na-KD_Prodhttp_JMeter_PhantomJS.jmx -DPhantomJS_Driver="$(which phantomjs)" -l /opt/jmeter/report.jtl -Grun.properties
