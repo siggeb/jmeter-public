5b55c6e29c75018900de2e7aea02be6f7de3960c
Jakob Hellsten <jakob.hellsten@avensia.com>
2017-11-14 18:18:04 +0100

    Update for PhantomJs_Template

---
+++ elasticsearch-jmeter/testpack/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm /opt/jmeter/report.jtl
+rm -r /opt/jmeter/html_report
+JVM_ARGS="-Xms1024m -Xmx6144m -XX:NewSize=512m -XX:MaxNewSize=6144m" && export JVM_ARGS && /opt/jmeter/apache-jmeter-3.1/bin/jmeter -n -r -t /opt/jmeter/JMeter_PhantomJS_Template.jmx -DPhantomJS_Driver="$(which phantomjs)" -l /opt/jmeter/report.jtl -Grun.properties
