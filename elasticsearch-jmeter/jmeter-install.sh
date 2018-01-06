cddc622c4201bcde2aa38e2353c9fc30ed19684a
Jakob Hellsten <jakobhellsten@users.noreply.github.com>
2018-01-06 15:08:55 +0100

    Update jmeter-install.sh

--- elasticsearch-jmeter/jmeter-install.sh
+++ elasticsearch-jmeter/jmeter-install.sh
@@ -32,6 +32,7 @@ help()
     echo "  -h              view this help content"
     echo "  -m              install as a master node"
     echo "  -r <hosts>      set remote hosts (master only)"
+    echo "  -j <jarball>    location of the jarball to download and unzip"
     echo "  -t <testpack>   location of the test pack to download and unzip (master only)"
 }
 
@@ -69,6 +70,9 @@ while getopts :hmr:j:t: optname; do
     r) # provide remote hosts
       REMOTE_RANGE=${OPTARG}
       ;;
+    j) # provide jarball
+      JARBALL=${OPTARG}
+      ;;
     t) # provide testpack
       TESTPACK=${OPTARG}
       ;;
@@ -97,12 +101,10 @@ install_java()
 {
     log "Installing Java"
     add-apt-repository -y ppa:webupd8team/java
-    mkdir -p /var/cache/oracle-jdk8-installer
-    wget -q http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u144-linux-x64.tar.gz -O /var/cache/oracle-jdk8-installer/jdk-8u144-linux-x64.tar.gz
-    apt-get -y update
+    apt-get -y update  > /dev/null
     echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
     echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-    apt-get -y install oracle-java8-installer
+    apt-get -y install oracle-java8-installer  > /dev/null
 }
 
 install_jmeter_service()
@@ -112,7 +114,7 @@ install_jmeter_service()
 
     start on starting
     script
-        JVM_ARGS="-Xms1024m -Xmx6144m -XX:NewSize=512m -XX:MaxNewSize=6144m" && export JVM_ARGS && /opt/jmeter/apache-jmeter-3.1/bin/jmeter-server
+        /opt/jmeter/apache-jmeter-2.13/bin/jmeter-server
     end script
 EOF
 
@@ -122,14 +124,14 @@ EOF
 
 update_config_sub()
 {
-    mv /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties.bak
-    cat /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties.bak | sed "s|#client.rmi.localport=0|client.rmi.localport=4441|" | sed "s|#server.rmi.localport=4000|server.rmi.localport=4440|" > /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties 
+    mv /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties.bak
+    cat /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties.bak | sed "s|#client.rmi.localport=0|client.rmi.localport=4441|" | sed "s|#server.rmi.localport=4000|server.rmi.localport=4440|" > /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties 
 }
 
 update_config_boss()
 {
-    mv /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties.bak
-    cat /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties.bak | sed "s|#client.rmi.localport=0|client.rmi.localport=4440|" | sed "s|remote_hosts=127.0.0.1|remote_hosts=${REMOTE_HOSTS}|" > /opt/jmeter/apache-jmeter-3.1/bin/jmeter.properties 
+    mv /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties.bak
+    cat /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties.bak | sed "s|#client.rmi.localport=0|client.rmi.localport=4440|" | sed "s|remote_hosts=127.0.0.1|remote_hosts=${REMOTE_HOSTS}|" > /opt/jmeter/apache-jmeter-2.13/bin/jmeter.properties 
 }
 
 install_jmeter()
@@ -138,22 +140,24 @@ install_jmeter()
     apt-get -y install unzip 
     
     mkdir -p /opt/jmeter
-    wget -O jmeter.zip https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-3.1.zip
-    wget -O JMeterPlugins-Standard-1.4.0.zip http://jmeter-plugins.org/downloads/file/JMeterPlugins-Standard-1.4.0.zip
-	wget -O JMeterPlugins-WebDriver-1.4.0.zip http://jmeter-plugins.org/downloads/file/JMeterPlugins-WebDriver-1.4.0.zip
-	wget -O JMeterPlugins-ExtrasLibs-1.4.0.zip https://jmeter-plugins.org/downloads/file/JMeterPlugins-ExtrasLibs-1.4.0.zip
+    wget -O jmeter.zip http://apache.mirror.anlx.net/jmeter/binaries/apache-jmeter-2.13.zip
+    wget -O plugins.zip http://jmeter-plugins.org/downloads/file/JMeterPlugins-Standard-1.3.0.zip
     
     log "unzipping jmeter"
     unzip -q jmeter.zip -d /opt/jmeter/
     
     log "unzipping plugins"
-    unzip -q JMeterPlugins-Standard-1.4.0.zip -d /opt/jmeter/apache-jmeter-3.1/
-	unzip -q JMeterPlugins-WebDriver-1.4.0.zip -d /opt/jmeter/apache-jmeter-3.1/
-	unzip -q JMeterPlugins-ExtrasLibs-1.4.0.zip -d /opt/jmeter/apache-jmeter-3.1/
+    unzip -q plugins.zip -d /opt/jmeter/apache-jmeter-2.13/
      
-    chmod u+x /opt/jmeter/apache-jmeter-3.1/bin/jmeter-server
-    chmod u+x /opt/jmeter/apache-jmeter-3.1/bin/jmeter
+    chmod u+x /opt/jmeter/apache-jmeter-2.13/bin/jmeter-server
+    chmod u+x /opt/jmeter/apache-jmeter-2.13/bin/jmeter
 
+    if [ ${JARBALL} ];
+    then
+        log "installing jarball"
+        wget -O jarball.zip ${JARBALL}
+        unzip -q jarball.zip -d /opt/jmeter/apache-jmeter-2.13/lib/junit/
+    fi
     
     if [ ${IS_MASTER} -ne 1 ]; 
     then
@@ -174,8 +178,6 @@ install_jmeter()
             log "installing test pack"
             wget -O testpack.zip ${TESTPACK}
             unzip -q testpack.zip -d /opt/jmeter/
-            cp /opt/jmeter/testpack/* /opt/jmeter/
-            rm -r /opt/jmeter/testpack/
             chmod +x /opt/jmeter/run.sh
         fi
     fi
@@ -183,13 +185,6 @@ install_jmeter()
     groupadd -g 999 jmeter
     useradd -u 999 -g 999 jmeter
     chown -R jmeter: /opt/jmeter
-}
-install_phantomjs()
-{
-    log "Installing phantomjs"
-    apt-get -y update  > /dev/null
-    apt-get -qy install wget default-jre-headless telnet iputils-ping unzip nodejs-legacy npm  > /dev/null
-	npm install -g phantomjs > /dev/null
 }
 
 
@@ -201,7 +196,6 @@ then
 fi
 
 install_java
-install_phantomjs
 install_jmeter
 
 log "script complete"
