cea7aed2c839e79dbde0d9889c961a55f38cc769
Sigurdur Birgisson <siggge@gmail.com>
2018-10-04 12:55:46 +0200

    Cleanup repo before upgrade to Jmeter 4

--- elasticsearch-jmeter/testpack/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm /opt/jmeter/report.jtl
-rm -r /opt/jmeter/html_report
-JVM_ARGS="-Xms1024m -Xmx6144m -XX:NewSize=512m -XX:MaxNewSize=6144m" && export JVM_ARGS && /opt/jmeter/apache-jmeter-3.1/bin/jmeter -n -r -t /opt/jmeter/JMeter_PhantomJS_Template.jmx -DPhantomJS_Driver="/usr/local/bin/phantomjs.exe" -l /opt/jmeter/report.jtl -Grun.properties
