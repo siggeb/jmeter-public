44dab407c1f4b57e339f3ea0e912e778fe0e096d
Johan Björklund <johan.bjorklund@avensia.com>
2018-10-17 14:15:03 +0200

    temp fix for java repo

--- elasticsearch-jmeter/jmeter-install.sh
+++ elasticsearch-jmeter/jmeter-install.sh
@@ -99,10 +99,14 @@ install_java()
     add-apt-repository -y ppa:webupd8team/java
     mkdir -p /var/cache/oracle-jdk8-installer
     wget -q http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u144-linux-x64.tar.gz -O /var/cache/oracle-jdk8-installer/jdk-8u144-linux-x64.tar.gz
-    apt-get -y update
+    sudo apt-get -y update
     echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
     echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-    apt-get -y install oracle-java8-installer
+    cd /var/lib/dpkg/info && sudo sed -i 's|JAVA_VERSION=8u181|JAVA_VERSION=8u191|' oracle-java8-installer.*
+    sudo sed -i 's|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/|' oracle-java8-installer.*
+    sudo sed -i 's|SHA256SUM_TGZ=.*|SHA256SUM_TGZ="53c29507e2405a7ffdbba627e6d64856089b094867479edc5ede4105c1da0d65"|' oracle-java8-installer.*
+    sudo sed -i 's|J_DIR=jdk1.8.0_181|J_DIR=jdk1.8.0_191|' oracle-java8-installer.*
+    sudo apt-get -y install oracle-java8-installer
 }
 
 install_jmeter_service()
