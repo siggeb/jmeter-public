a3c81823df542c4d92b2343090758c559e9a5fdf
Sigurdur Birgisson <siggge@gmail.com>
2018-10-24 11:02:47 +0200

    New rmi config in user.properties

--- elasticsearch-jmeter/build_testpack.sh
+++ elasticsearch-jmeter/build_testpack.sh
@@ -4,5 +4,5 @@ rm -r testpack/
 rm testpack.zip
 
 mkdir -p testpack
-cp resources/{run.properties,run.sh,*.jmx} testpack/
+cp resources/{run.properties,run.sh,*.jmx,user.properties,rmi_keystore.jks} testpack/
 zip -r -X testpack.zip testpack
